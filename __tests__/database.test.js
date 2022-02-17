e13b7a511a59d7efa3469e80e06a91196eedee6d
Damog83 <damiengittings@hotmail.co.uk>
2022-02-17 14:33:47 +0000

    something

--- __tests__/database.test.js
+++ __tests__/database.test.js
@@ -61,15 +61,17 @@ describe('/api/articles/:article_id', () => {
             .send(incVotes)
             .expect(200)
             .then((response) => {
-                expect(response.body.articleObj).toEqual({
-                    author: 'butter_bridge',
-                    title: 'Living in the shadow of a great man',
-                    article_id: 1,
-                    body: 'I find this existence challenging',
-                    topic: 'mitch',
-                    created_at: '2020-07-09T20:11:00.000Z',
-                    votes:110
-                })
+                expect(response.body.articleObj).toEqual(
+                    expect.objectContaining({
+                            author: expect.any(String),
+                            title: expect.any(String),
+                            article_id: expect.any(Number),
+                            body: expect.any(String),
+                            topic: expect.any(String),
+                            created_at: expect.any(String),
+                            votes: expect.any(Number)
+                    })
+                )
             })
         })
     })
