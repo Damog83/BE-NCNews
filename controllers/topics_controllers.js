320ab63f31dab35527ce0c439697ef2c9cb477e3
Damog83 <damiengittings@hotmail.co.uk>
2022-08-11 18:49:12 +0100

    format topics controllers

--- controllers/topics_controllers.js
+++ controllers/topics_controllers.js
@@ -1,10 +1,10 @@
-const {fetchTopics} = require('../models/topics_models.js')
+const { fetchTopics } = require('../models/topics_models.js');
 
 exports.getTopics = (req, res, next) => {
-    const {sort, order} = req.query;  
-     fetchTopics(sort, order).then((topics) => {
-       res.status(200).send({topics});
-     }).catch(next)
-  }
-
-
+	const { sort, order } = req.query;
+	fetchTopics(sort, order)
+		.then((topics) => {
+			res.status(200).send({ topics });
+		})
+		.catch(next);
+};
