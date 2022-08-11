49cac6020a7f09e69dd1b26d720259cde68e5c0d
Damog83 <damiengittings@hotmail.co.uk>
2022-08-11 18:52:50 +0100

    format customerrors PGSQLerrors and servererrors

--- errors/customerrors.js
+++ errors/customerrors.js
@@ -1,7 +1,5 @@
-
-
 exports.handleCustomErrors = (err, req, res, next) => {
-    if (err.status && err.msg) {
-      res.status(err.status).send({msg: `${err.msg}`});
-    } else next(err);
-  };
+	if (err.status && err.msg) {
+		res.status(err.status).send({ msg: `${err.msg}` });
+	} else next(err);
+};
--- errors/servererrors.js
+++ errors/servererrors.js
@@ -1,6 +1,4 @@
-
-
 exports.handleServerErrors = (err, req, res, next) => {
-  console.log(err)
-    res.status(500).send({ msg: 'Internal Server Error' });
-  };
+	console.log(err);
+	res.status(500).send({ msg: 'Internal Server Error' });
+};
